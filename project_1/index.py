d5b1896d0faca3dbdbff929ff93280ece8dcdb09
roberto <roberto.judet@itschool.ro>
2022-02-10 16:19:37 +0200

    last changes

--- project_1/index.py
+++ project_1/index.py
@@ -1,17 +1,24 @@
 # this is our root file, when we execute this we will start our app
-import json
-from fastapi import FastAPI
+# webpage for fastapi framework https://fastapi.tiangolo.com/tutorial/,
+# also https://realpython.com/fastapi-python-web-apis/
 
-app = FastAPI(
-    title="Name of our app",
-    # <major_version>.<minor_version>.<patch_version>
-    version="0.2.0",
-)
-# uvicorn index:app --reload --post 7777
+# uvicorn index:app --reload --port 7777
 # uvicorn is the server which will start
 # index:app, index -> the file name, app -> the FastAPI object name
 # --reload -> the server will restart when we modify the code
-# --post <port_number> -> select on which port to start
+# --port <port_number> -> select on which port to start
+
+
+from fastapi import FastAPI
+from stock import Stock
+from stock_repo import StockRepository
+
+app = FastAPI(
+    title="Name of our app",  # TODO for homework, name your application
+    # <major_version>.<minor_version>.<patch_version>
+    version="0.2.0",  # increase version after finishing homework
+    description="",  # TODO add a description
+)
 
 
 @app.get(
@@ -26,26 +33,35 @@ def health() -> dict:
         "engine": "on",
     }
 
-list_of_items = []
 
-@app.post("/items")
-def add_new_item(request: dict):
-    list_of_items.append(request)
-    list_json = json.dumps(list_of_items)
-    file = open("database.txt", "w")
-    file.write(list_json)
-    file.close()
+stock_repo = StockRepository()
 
 
-@app.get("/items")
-def get_items():
-    return list_of_items
+@app.post("/stocks")
+def add_new_stock(stock_info: dict):
+    new_stock = Stock(stock_info["ticker"], stock_info["company"], stock_info["field"])
+    stock_repo.add(new_stock)
+
+
+# TODO return your domain items
+# example if you want to do a tasks app return the list of tasks, and rename the url /items -> /tasks
+@app.get("/stocks")
+def get_stocks():
+    stocks = stock_repo.get_all()
+    return [s.to_json() for s in stocks]
+
+
+# TODO add a put method to edit your domain item
+# TODO add a delete method to remove a domain item from the list
+# these methods should also save the data into a file for persistence across server reboots
+
+
+@app.delete("/stocks")
+def remove_stock(ticker: str):
+    stock_repo.remove(ticker)
 
 
 @app.on_event("startup")
 def load_list_of_items():
-    file = open("database.txt")
-    json_items = file.read()
-    file.close()
-    list_of_items.append(json.loads(json_items))
+    stock_repo.load()
 
